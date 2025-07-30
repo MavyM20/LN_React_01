5264df5629fa31884ebf266d95995820bade678b
MavyM20 <222101232+MavyM20@users.noreply.github.com>
2025-07-30 17:07:43 -0500

    section 4, video 26, useState-hook

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
 import PropTypes from "prop-types";
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = (event) => {
+    //console.log(event);
+    //setCounter(counter + 1); //Le dice que el estado cambio
+    setCounter((c) => c + 1); //Otra forma para incrementar el counter
+  };
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
+
+      <button onClick={handleAdd}>+1</button>
     </>
   );
 };
